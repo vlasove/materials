92ac4f15aeed95e47437da69d7dd6c5d2615d2f0
vlasove <evgeny_vlasov@yahoo.com>
2021-06-03 15:57:16 +0300

    added todos

--- bytest/s1/main.go
+++ bytest/s1/main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+// Todo: add PostgresPlayerStore
+// Todo: add context
+
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":8080", server))
