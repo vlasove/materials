48c14127c240e8b4a6630cbf5bc9063f687d90de
vlasove <evgeny_vlasov@yahoo.com>
2021-06-03 12:05:20 +0300

    done f12

---
+++ bytest/f12/safer/safer_test.go
@@ -0,0 +1,39 @@
+package safer
+
+import (
+	"sync"
+	"testing"
+)
+
+func assertCounter(t *testing.T, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
+	}
+}
+func TestCounter(t *testing.T) {
+	t.Run("Increment the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("increment safely and cocnurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+	})
+}
