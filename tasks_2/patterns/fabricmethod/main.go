068b6a34d3079d628c29fe0d5523a3907bf76af1
vlasove <evgeny_vlasov@yahoo.com>
2021-07-29 14:00:15 +0300

    added fabric method

---
+++ tasks_2/patterns/fabricmethod/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/vlasove/materials/tasks_2/patterns/fabricmethod/gunfactory"
+)
+
+func main() {
+	ak47, err := gunfactory.GetGun("ak47", 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fmt.Println(ak47)
+
+	musket, err := gunfactory.GetGun("musket", 900)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fmt.Println(musket)
+}
